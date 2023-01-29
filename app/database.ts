f0601c685bb2c8fc71f087e9017926396c99a44f
Nicholas Bergesen <nicholasbergesen@github.com>
2023-01-29 20:54:54 +0900

    Add sqlite3 database to store logs

---
+++ app/database.ts
@@ -0,0 +1,66 @@
+import sqlite3 from 'sqlite3'
+import dotenv from 'dotenv'
+//import md5 from 'md5'
+
+class MyDatabase
+{
+    public connection: sqlite3.Database
+
+    constructor() {
+        dotenv.config();
+        const dbsource = <string>process.env.DBSOURCE
+        this.connection = new sqlite3.Database(dbsource, this.handleConnection)
+    }
+
+    private handleConnection = (err: Error | null) => {
+        if (err) {
+            console.error(err.message)
+            throw err
+        }
+
+        console.log('Connected to the SQLite database.')
+
+        //create user table
+        // db.run(`CREATE TABLE user (
+        //     id INTEGER PRIMARY KEY AUTOINCREMENT,
+        //     name text, 
+        //     email text UNIQUE, 
+        //     password text, 
+        //     CONSTRAINT email_unique UNIQUE (email)
+        //     )`,
+        // (err) => {
+        //     if (err) {
+        //         // Table already created
+        //     }else{
+        //         // Table just created, creating some rows
+        //         var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+        //         //db.run(insert, ["admin","admin@example.com",md5("admin123456")])
+        //         //db.run(insert, ["user","user@example.com",md5("user123456")])
+        //     }
+        // });  
+
+        //create log table
+        this.connection.run(`CREATE TABLE IF NOT EXISTS log (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            time STRING, 
+            message TEXT
+            )`)
+    }
+
+
+    // public write_log = (message: string) => {
+    //     const query = `INSERT INTO log (time, message) VALUES (?, ?)`
+    //     this.db.run(query, [new Date().toISOString(), message])
+    // }
+
+    // public read_log = () : string[] => {
+    //     const query = `SELECT * FROM log LIMIT 20`
+    //     const result: string[] = []
+    //     this.db.all(query, result)
+    //     return result
+    // }
+}
+
+const db = new MyDatabase
+
+export default db
--- app/index.ts
+++ app/index.ts
@@ -1,16 +1,38 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import db from './database'
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
-const url = process.env.URL;
+const app: Express = express()
+const port = process.env.PORT
+const url = process.env.URL
+
+app.use('/', express.text())
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+  //console.log(`called from ${req.ip}`)
+  //db.write_log(`called from ${req.ip}`)
+  // res.write(`${req.ip}\r\n`)
+
+  // for (let i = 0; i < req.rawHeaders.length; i+=2) {
+  //   const name = req.rawHeaders[i]
+  //   const value = req.rawHeaders[i + 1]
+
+  //   res.write(`${name}:${value}`)
+  //   res.write('\r\n')
+  // }
+  // res.write(JSON.stringify(req.body))
+  //const logs = db.read_log();
+
+  db.connection.all(`SELECT * FROM log LIMIT 20`, (_: Error, rows: string[]) => {
+    rows.forEach(row => {
+      res.write(`${JSON.stringify(row)}\r\n`)
+    })
+    res.end()
+  })
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://${url}:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at http://${url}:${port}`)
+})
